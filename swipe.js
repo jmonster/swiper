41f0bd850314a9a0e4023657d89e99b54c8eb141
Johnny Domino <johnny@brandingbrand.com>
2013-12-06 14:49:39 -0500

    crawl with Googlebot useragent

--- swipe.js
+++ swipe.js
@@ -15,7 +15,8 @@ var request = require('request').defaults({
   'headers':{
     'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Language':'en-US,en;q=0.8,es;q=0.6',
-    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36'
+    // 'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36'
+    'User-Agent':'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
   }
 });
 

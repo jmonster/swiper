838d707ae4bc031dcc686050d93f16b2db463b68
Angelo Di Paolo <angelod101@gmail.com>
2014-08-22 18:35:10 -0400

    started ulta recipe

---
+++ recipes/ulta.js
@@ -0,0 +1,26 @@
+/**
+  ulta
+  todo: price on pdp is fucking ajax brah
+*/
+
+exports.conf =
+  {
+    "root": "http://www.ulta.com/ulta/common/sitemap.jsp",
+    "next": {
+      "select": ".secondLevelWrapper a",
+      "next": {
+        "collect": {
+          "selector": ".next-prev a"
+        },
+        "select": ".prod-title a",
+        "next": {
+          "stash": {
+            "namespace": "hayneedle",
+            "extract": {
+              "price": "#skuInfoPrice"
+            }
+          }
+        }
+      }
+    }
+  };

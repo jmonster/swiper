fda1fd93a86af7cd7a1e36fd202a184e365ca3e4
Angelo Di Paolo <angelod101@gmail.com>
2014-08-21 20:50:28 -0400

    add walgreens

---
+++ recipes/walgreens.js
@@ -0,0 +1,28 @@
+/**
+  walgreens
+*/
+
+exports.conf =
+  {
+    "root": "http://www.walgreens.com/site_map.jsp",
+    "next": {
+      "select" : "#shop_content li[class!=first] a",
+      "next": {
+        "select": "#sidenav a",
+        "next": {
+          "collect": {
+            "selector": "#pagination a[title='Next Page']"
+          },
+          "select": ".product-name a",
+          "next": {
+            "stash": {
+              "namespace": "walgreens",
+              "extract":{
+                "price": ["#sale_amount", "#vpdSinglePrice", "#main [itemprop='price']", , "#price_amount"]
+              }
+            }
+          }
+        }
+      }
+    }
+  };

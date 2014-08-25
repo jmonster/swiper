4096326f3a3013c2ede37ae7a3da814e61ce1ae7
Angelo Di Paolo <angelod101@gmail.com>
2014-08-24 23:02:55 -0400

    add macy's recipe

---
+++ recipes/macys.js
@@ -0,0 +1,26 @@
+
+/**
+  macy's
+*/
+
+exports.conf =
+  {
+    "root": "http://www1.macys.com/cms/slp/2/Site-Index",
+    "next": {
+      "select": "#sitemap_wrapper a",
+      "next": {
+        "collect": {
+          "selector": ".arrowRight"
+        },
+        "select": ".productThumbnailLink",
+        "next": {
+          "stash": {
+            "namespace": "macys",
+            "extract": {
+              "price": ["#priceInfo .priceSale", ".standardProdPricingGroup span:first-child"]
+            }
+          }
+        }
+      }
+    }
+  };

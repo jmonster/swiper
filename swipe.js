10446e1c71a264b8b713daa5cb62b651fc49fd50
Johnny Domino <jvdomino@gmail.com>
2014-08-25 14:21:17 -0400

    subscribe to scutter's `done` and `error` events

--- swipe.js
+++ swipe.js
@@ -13,10 +13,31 @@ var conf   = recipe.conf;
 var Scutter = require('./lib/scutter');
 var scutter = new Scutter(recipe);
 
-scutter.onComplete = function() {
+scutter.on('done',function() {
   console.log('swiping completed'.blue);
   console.timeEnd('swiping');
-};
+});
+
+if (process.env.NODE_ENV === 'production') {
+  // Rollbar
+  var rollbar = require('rollbar');
+  var rollbar_token = 'a8c70478b8ec473195a13927a3cb9a97';
+  rollbar.init(rollbar_token);
+  rollbar.handleUncaughtExceptions(rollbar_token);
+
+  scutter.on('error',function(error,additionalValues) {
+    if (additionalValues) {
+      rollbar.handleErrorWithPayloadData(error, {'custom': additionalValues});
+    }
+    else {
+      rollbar.handleError(error);
+    }
+  });
+} else { // DEVELOPMENT
+  scutter.on('error',function(error,additionalValues) {
+    console.error(error,additionalValues);
+  });
+}
 
 console.log('Swiper, no swiping!'.green.bold);
 console.log(JSON.stringify(conf,null,2).yellow+'\r\n');  

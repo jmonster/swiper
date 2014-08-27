b6d19d8f1f99685099048b9a04662f552aaac3f3
Johnny Domino <johnny@yayuhh.com>
2014-08-26 23:32:46 -0400

    support pagination/infinite scrolling on overstock product index pages

--- recipes/overstock.js
+++ recipes/overstock.js
@@ -1,8 +1,3 @@
-/**
-  overstock.com
-  todo: swipe pagination (ajax)
-*/
-
 exports.conf =
   {
     "namespace": "overstock",
@@ -10,6 +5,10 @@ exports.conf =
     "next": {
       "select" : ".bullet3 a",
       "next": {
+        "before": resolvePagination,
+        "collect": {
+          "selector":"body > a.swiper.pagination" // relies on resolvePagination -- see below!
+        },
         "select": ".product a",
         "next" : {
           "stash": {
@@ -21,3 +20,38 @@ exports.conf =
       }
     }
   };
+
+
+var request = require('request');
+var cheerio = require('cheerio');
+var Url     = require('url');
+
+var PER_PAGE = 120; // max supported on overstock
+
+
+function resolvePagination(response,html,done) {
+  var $ = cheerio.load(html,{ decodeEntities: false }); // `&` in querystrings get messed up without decodeEntities:false
+  var currentUri = response.request.uri;
+  currentUri.query = {infinite: true};
+
+  // determine how many "pages" we need to fetch
+  request(Url.format(currentUri), function(error, response, body) {
+    var blob = JSON.parse(body);
+    var totalResults = blob.totalResults;
+    var pageCount = totalResults/PER_PAGE;
+    var html2 = $('body');
+
+    // create array of URLs
+    for (i = 1; pageCount > 1 && i < pageCount; i++) { // skip the first page since it's already processed
+      delete currentUri.search; // overrules query!
+      currentUri.query = { infinite: true, index: i*PER_PAGE };
+      var uri = Url.format(currentUri);
+
+      // inject those URLs back into the page
+      var linkTag = '<a class="swiper pagination" href="'+uri+'"></a>';
+      html2.append(linkTag);
+    }
+
+    done(null,$.html());
+  });
+}

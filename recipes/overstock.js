3560b685114b591facbed98a87a8b93e6b803e07
Nick Tate <nick.tate@brandingbrand.com>
2014-10-16 16:17:03 -0400

    error handle in overstock recipe

--- recipes/overstock.js
+++ recipes/overstock.js
@@ -43,7 +43,13 @@ function resolvePagination(response,html,done) {
       return done();
     }
     
-    var blob = JSON.parse(body);
+    var blob;
+    try {
+      blob = JSON.parse(body);
+    } catch(e) {
+      return done(e);
+    }
+
     var totalResults = blob.totalResults;
     var pageCount = totalResults/PER_PAGE;
     var html2 = $('body');

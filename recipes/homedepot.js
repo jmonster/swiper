b03076a7c39d3db0b8b9fd3159830bf39d1ec652
Nick Tate <nick.tate@brandingbrand.com>
2014-11-09 13:25:08 -0500

    added home depot

--- recipes/homedepot.js
+++ recipes/homedepot.js
@@ -1,21 +1,24 @@
 exports.conf =
   {
     "namespace": "homedepot",
-    "root": "http://www.homedepot.com/",
+    "root": "http://www.homedepot.com/site-directory/index.html",
+    "minimalQueryString": [],
     "next": {
-      "select": ".L1",
+      "select": "li~ li+ li li a",
       "next": {
-        "select": ".activeLevel a",
+        "select": "a.item_description",
+        "collect": {
+          "selector": ".icon-next"
+        },
         "next": {
-          "select": ".item_description",
-          "collect": {
-            "selector": ".icon-next"
-          },
-          "next": {
-            "stash": {
-              "extract": {
-                "price": "#ajaxPrice"
-              }
+          "stash": {
+            "extract": {
+              "price": "span[itemprop=price]#ajaxPrice",
+              "name": "meta[property='og:title']...attr:content",
+              "description": "meta[name=description]...attr:content",
+              "image": "meta[property='og:image']...attr:content",
+              "sku": "span[itemprop=sku]",
+              "model": "h2.modelNo"
             }
           }
         }

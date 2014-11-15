2d6afed30492f597f7efea16ee1d6f02240b116b
Johnny Domino <johnny@yayuhh.com>
2014-11-14 22:11:55 -0500

    use ENV variables for params.js

--- params.js
+++ params.js
@@ -1,26 +1,29 @@
+var ENV = process.env;
+
 var os = require('os');
 var optimist = require('optimist')
 
-.usage('Usage : $0 --recipe [merchant]')
+optimist
+  .usage('Usage : $0 --recipe [merchant]')
 
-.describe ('recipe', 'merchant recipe to apply')
-.describe ('db', 'storage engine to persist results')
-.default  ('db', 'http')
-.describe ('batch-size', 'number of extracted values to batch')
-.default  ('batch-size', 250)
-.describe ('http-endpoint', 'URL endpoint for POSTing prices')
-.default  ('http-endpoint', 'http://localhost:8000')
-.describe ('es-endpoint', 'URL endpoint for elasticsearch')
-.default  ('es-endpoint', 'http://elasticsearch.yayuhh.com:9200')
-.describe ('mongo-endpoint', 'URL endpoint for elasticsearch')
-.default  ('mongo-endpoint', 'mongodb://test:test@74.111.162.81:27018/test')  
-.describe ('catalog-path', 'path to store pdp html files')
-.default  ('catalog-path', os.tmpdir())
-.describe ('concurrency', 'number of simultaneous requests to make')
-.default  ('concurrency', 4)
-.describe ('help', 'these usage instructions')
-.alias    ('help', 'h')
-;
+  .describe ('recipe', 'merchant recipe to apply')
+  .describe ('db', 'storage engine to persist results')
+  .default  ('db', 'http')
+  .describe ('batch-size', 'number of extracted values to batch')
+  .default  ('batch-size', ENV.BATCH_SIZE || 250)
+  .describe ('http-endpoint', 'URL endpoint for POSTing prices')
+  .default  ('http-endpoint', ENV.API_URL || 'http://localhost:8000')
+  .describe ('es-endpoint', 'URL endpoint for elasticsearch')
+  .default  ('es-endpoint', ENV.ELASTIC_SEARCH_URL || 'http://elasticsearch.yayuhh.com:9200')
+  .describe ('mongo-endpoint', 'URL endpoint for elasticsearch')
+  .default  ('mongo-endpoint', ENV.MONGO_URL || 'mongodb://test:test@74.111.162.81:27018/test')  
+  .describe ('catalog-path', 'path to store pdp html files')
+  .default  ('catalog-path', os.tmpdir())
+  .describe ('concurrency', 'number of simultaneous requests to make')
+  .default  ('concurrency', ENV.CONCURRENCY || 4)
+  .describe ('help', 'these usage instructions')
+  .alias    ('help', 'h')
+  ;
 
 var argv = optimist.argv;
 

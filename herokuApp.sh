248d065d27ae09488e4e566e3b7d3d60cc933b21
nick tate <ntate22@gmail.com>
2014-11-16 01:25:07 -0500

    30 char name limit

--- herokuApp.sh
+++ herokuApp.sh
@@ -2,7 +2,12 @@
 
 for recipe in recipes/*.js(:t:r); do
  app=yayuhh-swiper-$recipe
- git remote add $app git@heroku.swiper:$app.git
- git push $app master:master
- git remote remove $app
+ truncated_app=${app:0:30}
+ git remote add $truncated_app git@heroku.swiper:$truncated_app.git
+
+ # recipe env variable config
+ heroku config:set RECIPE=$recipe -a $truncated_app
+
+ git push $truncated_app master:master
+ git remote remove $truncated_app
 done
